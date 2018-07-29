b1e818b45ee6cec61de24aa00d5dd05e044bf353
xiaqijian <2817062995@qq.com>
2018-07-29 09:47:58 +0800

    添加forbreak

---
+++ breack.py
@@ -0,0 +1,8 @@
+# filename: break.py
+
+while True:
+    s = raw_input('enter someting')
+    if s=='quir':
+        break
+    print( 'length of the string')
+print('done')
---
+++ for.py
@@ -0,0 +1,6 @@
+# filename: for.py
+
+for i in range(1,5):
+    print(i)
+else:
+    print('哈哈哈')
