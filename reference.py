5111d0111f7e2618abc146712188d7c6b18ea0f6
xiaqijian <2817062995@qq.com>
2018-08-03 10:21:21 +0800

    referce

---
+++ reference.py
@@ -0,0 +1,9 @@
+# filename: reference.py
+
+print('simple a ssignment')
+shoplist= ['apple','mango','carrot','banana']
+mylist = shoplist
+
+del shoplist[0]
+print('shoplist',shoplist)
+print('mylist',mylist)
